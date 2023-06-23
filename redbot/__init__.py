9d91a1129fea75958e0532308b9475cdd92efd6f
Karlo Prikratki <karlo@karloprikratki.com>
2023-06-23 16:06:52 +0200

    messing around

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -205,10 +205,12 @@ class VersionInfo:
 
     @classmethod
     def _get_version(cls, *, ignore_installed: bool = False) -> _Tuple[str, "VersionInfo"]:
+        global _VERSION
         if not _VERSION.endswith(".dev1"):
-            _VERSION += "-konjanik"
+            _VERSION = f"{_VERSION}-konjanik"
             return _VERSION, cls.from_str(_VERSION)
-        _VERSION += "-konjanik"
+        _VERSION = f"{_VERSION}-konjanik"
+        
 
         project_root = _os.path.abspath(_os.path.dirname(_os.path.dirname(__file__)))
 
